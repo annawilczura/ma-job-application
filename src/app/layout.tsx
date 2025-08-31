b9a3177de44d5e64dcdd89356217ef2f1cce2445
annawilczura <98234888+annawilczura@users.noreply.github.com>
2025-08-31 22:13:30 +0200

    fix: accessibility issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import { PrimeReactProvider } from 'primereact/api';
+import type { Metadata } from 'next';
 
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'MA Job Application',
+  description: 'Job application project for MA',
+};
 
 export default function RootLayout({
   children,
