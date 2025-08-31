df1dc34fbebda078a542c63728c17072390caaa0
annawilczura <98234888+annawilczura@users.noreply.github.com>
2025-08-31 21:04:55 +0200

    chore: update footer

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -9,6 +9,7 @@ interface AppLayoutProps {
 }
 
 export default function AppLayout({ children }: AppLayoutProps) {
+  const currentYear = new Date().getFullYear();
   return (
     <div className='min-h-screen surface-ground flex flex-col'>
       <Navbar />
@@ -19,7 +20,10 @@ export default function AppLayout({ children }: AppLayoutProps) {
       {/* Footer */}
       <footer className='surface-900 text-surface-0 py-8 px-4 sm:px-6 lg:px-8'>
         <div className='max-w-4xl mx-auto text-center'>
-          <p>&copy; 2024 {texts.navigation.title}. Job Application Website.</p>
+          <p>
+            &copy; {currentYear} {texts.navigation.title}.{' '}
+            {texts.footer.subtitle}
+          </p>
         </div>
       </footer>
     </div>
